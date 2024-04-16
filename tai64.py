f8672b474bd0639cfd0a457735b40b27b781f922
Alex Willmer <alex@moreati.org.uk>
2024-04-16 13:43:24 +0100

    Inherit tain & taia from tai, consolidate methods

--- tai64.py
+++ tai64.py
@@ -27,11 +27,10 @@ class tai:
     _sec: int
 
     def __new__(cls, sec:int):
-        sec = int(sec)
-        if not MIN <= sec <= MAX:
-            raise ValueError(f'sec must be in 0..2**63-1', sec)
         self = object.__new__(cls)
-        self._sec = sec
+        self._sec = int(sec)
+        if not MIN <= self._sec <= MAX:
+            raise ValueError('sec must be in 0..2**63-1', self._sec)
         return self
 
     @classmethod
@@ -41,8 +40,8 @@ class tai:
 
     @classmethod
     def unpack(cls, buf:Buffer) -> Self:
-        sec, = cls._struct.unpack(buf)
-        return cls(sec)
+        args = cls._struct.unpack(buf)
+        return cls(*args)
 
     @property
     def sec(self) -> int:
@@ -52,7 +51,8 @@ class tai:
         return self.pack().hex()
 
     def pack(self) -> bytes:
-        return self._struct.pack(self.sec)
+        args = self._tuple()
+        return self._struct.pack(*args)
 
     def _tuple(self) -> tuple[int]:
         return (self._sec,)
@@ -70,52 +70,30 @@ class tai:
     def __float__(self):
         return float(self.sec)
 
-    def __int__(self):
-        return self.sec
-
     def __hash__(self):
-        # TODO Should hash(tai(...)) == hash(taia(..., 0, 0))?
-        return hash((self.__class__, self.sec))
+        return hash((self.__class__, *self._tuple()))
 
     def __repr__(self):
-        return f'{self.__class__.__module__}.{self.__class__.__name__}({self.sec})'
+        cls = self.__class__
+        args = ', '.join(f'{arg}' for arg in self._tuple())
+        return f'{cls.__module__}.{cls.__name__}({args})'
 
 
-class tain:
-    __slots__ = ('_sec', '_nano')
+class tain(tai):
+    __slots__ = ('_nano')
     _struct = struct.Struct('>QL')
-    _sec: int
     _nano: int
 
     def __new__(cls, sec:int, nano:int):
-        sec = int(sec)
-        nano = int(nano)
-        if not MIN <= sec <= MAX:
-            raise ValueError(f'sec must be in 0..2**63-1', sec)
-        if not 0 <= nano < 10**9:
-            raise ValueError(f'nano must be in 0..999_999_999', nano)
-        self = object.__new__(cls)
-        self._sec = sec
-        self._nano = nano
+        self = super().__new__(cls, sec)
+        self._nano = int(nano)
+        if not 0 <= self._nano <= 999_999_999:
+            raise ValueError('nano must be in 0..999_999_999', self._nano)
         return self
-
-    @classmethod
-    def from_hex(cls, s:str|Buffer) -> Self:
-        buf = binascii.a2b_hex(s[0:cls._struct.size*2])
-        return cls.unpack(buf)
 
     @classmethod
     def from_tai(cls, t:tai) -> Self:
         return cls(t.sec, 0)
-
-    @classmethod
-    def unpack(cls, buf:Buffer) -> Self:
-        sec, nano = cls._struct.unpack(buf)
-        return cls(sec, nano)
-
-    @property
-    def sec(self) -> int:
-        return self._sec
 
     @property
     def nano(self) -> int:
@@ -124,13 +102,7 @@ class tain:
     def frac(self) -> float:
         return self.nano/1e9
 
-    def hex(self) -> str:
-        return self.pack().hex()
-
-    def pack(self) -> bytes:
-        return self._struct.pack(self.sec, self.nano)
-
-    def _tuple(self) -> tuple[int, int]:
+    def _tuple(self) -> tuple[int, int]:  # type: ignore[override]
         return (self._sec, self._nano)
 
     def _compare(self, op, other) -> bool:
@@ -139,50 +111,21 @@ class tain:
         if typ is tai: return op(self._tuple(), (*other._tuple(), 0))
         return NotImplemented
 
-    def __eq__(self, other): return self._compare(operator.eq, other)
-    def __ge__(self, other): return self._compare(operator.ge, other)
-    def __gt__(self, other): return self._compare(operator.gt, other)
-    def __le__(self, other): return self._compare(operator.le, other)
-    def __lt__(self, other): return self._compare(operator.lt, other)
-
     def __float__(self):
         return float(self.sec) + self.frac()
 
-    def __hash__(self):
-        # TODO Should hash(tai(...)) == hash(taia(..., 0, 0))?
-        return hash((self.__class__, self.sec, self.nano))
 
-    def __repr__(self):
-        return f'{self.__class__.__module__}.{self.__class__.__name__}({self.sec}, {self.nano})'
-
-
-class taia:
-    __slots__ = ('_sec', '_nano', '_atto')
+class taia(tain):
+    __slots__ = ('_atto')
     _struct = struct.Struct('>QLL')
-    _sec: int
-    _nano: int
     _atto: int
 
     def __new__(cls, sec:int, nano:int, atto:int):
-        sec = int(sec)
-        nano = int(nano)
-        atto = int(atto)
-        if not MIN <= sec <= MAX:
-            raise ValueError(f'sec must be in 0..2**63-1', sec)
-        if not 0 <= nano < 10**9:
-            raise ValueError(f'nano must be in 0..999_999_999', nano)
-        if not 0 <= atto < 10**9:
-            raise ValueError(f'atto must be in 0..999_999_999', atto)
-        self = object.__new__(cls)
-        self._sec = sec
-        self._nano = nano
-        self._atto = atto
+        self = super().__new__(cls, sec, nano)
+        self._atto = int(atto)
+        if not 0 <= self._atto <= 999_999_999:
+            raise ValueError('atto must be in 0..999_999_999', self._atto)
         return self
-
-    @classmethod
-    def from_hex(cls, s:str|Buffer) -> Self:
-        buf = binascii.a2b_hex(s[0:cls._struct.size*2])
-        return cls.unpack(buf)
 
     @classmethod
     def from_tai(cls, t:tai) -> Self:
@@ -192,19 +135,6 @@ class taia:
     def from_tain(cls, t:tain) -> Self:
         return cls(t.sec, t.nano, 0)
 
-    @classmethod
-    def unpack(cls, buf:Buffer) -> Self:
-        sec, nano, atto = cls._struct.unpack(buf)
-        return cls(sec, nano, atto)
-
-    @property
-    def sec(self) -> int:
-        return self._sec
-
-    @property
-    def nano(self) -> int:
-        return self._nano
-
     @property
     def atto(self) -> int:
         return self._atto
@@ -212,13 +142,7 @@ class taia:
     def frac(self) -> float:
         return self.nano/1e9 + self.atto/1e18
 
-    def hex(self) -> str:
-        return self.pack().hex()
-
-    def pack(self) -> bytes:
-        return self._struct.pack(self.sec, self.nano, self.atto)
-
-    def _tuple(self) -> tuple[int, int, int]:
+    def _tuple(self) -> tuple[int, int, int]:  # type: ignore[override]
         return (self._sec, self._nano, self._atto)
 
     def _compare(self, op, other) -> bool:
@@ -227,22 +151,6 @@ class taia:
         if typ is tain: return op(self._tuple(), (*other._tuple(), 0))
         if typ is tai: return op(self._tuple(), (*other._tuple(), 0, 0))
         return NotImplemented
-
-    def __eq__(self, other): return self._compare(operator.eq, other)
-    def __ge__(self, other): return self._compare(operator.ge, other)
-    def __gt__(self, other): return self._compare(operator.gt, other)
-    def __le__(self, other): return self._compare(operator.le, other)
-    def __lt__(self, other): return self._compare(operator.lt, other)
-
-    def __float__(self):
-        return float(self.sec) + self.frac()
-
-    def __hash__(self):
-        # TODO Should hash(tai(...)) == hash(taia(..., 0, 0))?
-        return hash((self.__class__, self.sec, self.nano, self.atto))
-
-    def __repr__(self):
-        return f'{self.__class__.__module__}.{self.__class__.__name__}({self.sec}, {self.nano}, {self.atto})'
 
 
 __all__ = (
